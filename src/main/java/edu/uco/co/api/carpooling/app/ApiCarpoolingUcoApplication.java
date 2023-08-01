8f03ff8d03294179770dce1b9a725f6b07e46828
juan.cardona <felipe7u7432@gmail.com>
2023-07-31 20:37:15 -0500

    start proyect

---
+++ src/main/java/edu/uco/co/api/carpooling/app/ApiCarpoolingUcoApplication.java
@@ -0,0 +1,13 @@
+package edu.uco.co.api.carpooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarpoolingUcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarpoolingUcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uco/co/api/carpooling/ApiCarpoolingUcoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uco.co.api.carpooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarpoolingUcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
